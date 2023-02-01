de3191bbd006b7b256c282e7f10052d6577f7f73
Akshay Antony <akshaymuth@gmail.com>
2023-02-01 21:38:03 +0000

    working from 20:14

--- mobile_net.py
+++ mobile_net.py
@@ -25,6 +25,25 @@ class UpSample(nn.Module):
   def forward(self, x):
     return self.net(x)
 
+class Encoder(nn.Module):
+	def __init__(self):
+		super(Encoder, self).__init__()
+		self.layer = torchvision.models.mobilenet_v2(weights='MobileNet_V2_Weights.DEFAULT')
+
+
+	def forward(self, x):
+		features = [x]
+		temp = x
+		# for k, v in self.layer.features.named_children():
+		# 	features.append(v(features[-1]))
+		# print(len(features))
+		# return features
+		for layer_no, (k, v) in enumerate(self.layer.features.named_children()):
+			temp = v(temp)
+			if layer_no in [1, 2, 6, 13, 18]:
+				features.append(temp)
+		return features
+
 class Decoder(nn.Module):
 	def __init__(self, layer_list=[1280, 160, 96, 64, 32, 24, 16, 3]):
 		super(Decoder, self).__init__()
@@ -36,22 +55,11 @@ class Decoder(nn.Module):
 
 	def forward(self, features: List[torch.Tensor]):
 		x1 = self.decoder_1(features[-1]) #14*14
-		x2 = self.decoder_2(torch.cat((x1, features[14]), dim=1)) #28*28
-		x3 = self.decoder_3(torch.cat((x2, features[7]), dim=1)) #56*56
-		x4 = self.decoder_4(torch.cat((x3, features[4]), dim=1)) #112*112
-		x5 = self.decoder_5(torch.cat((x4, features[2]), dim=1)) #224*224
+		x2 = self.decoder_2(torch.cat((x1, features[-2]), dim=1)) #28*28
+		x3 = self.decoder_3(torch.cat((x2, features[-3]), dim=1)) #56*56
+		x4 = self.decoder_4(torch.cat((x3, features[-4]), dim=1)) #112*112
+		x5 = self.decoder_5(torch.cat((x4, features[-5]), dim=1)) #224*224
 		return x5 
-
-class Encoder(nn.Module):
-	def __init__(self):
-		super(Encoder, self).__init__()
-		self.layer = torchvision.models.mobilenet_v2(weights='MobileNet_V2_Weights.DEFAULT')
-
-	def forward(self, x):
-		features = [x]
-		for k, v in self.layer.features.named_children():
-			features.append(v(features[-1]))
-		return features
 
 class Mobile_Net_Unet(nn.Module):
 	def __init__(self):
--- tensorrt.py
+++ tensorrt.py
@@ -23,20 +23,21 @@ if __name__ == "__main__":
     model = Mobile_Net_Unet().eval().cuda()
     inputs_32 = [torch_tensorrt.Input((1, 3, 192, 224), dtype=torch.float32)]
     inputs_16 = [torch_tensorrt.Input((1, 3, 192, 224), dtype=torch.half)]
-    # trt_model_fp_32 = torch_tensorrt.compile(model, 
-    #                                          inputs=inputs_32, 
-    #                                          enabled_precisions={torch.float32},
-    #                                          require_full_compilation=False)
+    trt_model_fp_32 = torch_tensorrt.compile(model, 
+                                             inputs=inputs_32, 
+                                             enabled_precisions={torch.float32},
+                                             require_full_compilation=False)
     # trt_model_fp_16 = torch_tensorrt.compile(model,
     #                                          inputs=inputs_16,
-    #                                          enabled_precisions={torch.half, torch.float})
-    # trt_model_fp_32.save('mobilenet_32.ts')
+    #                                         #  truncate_long_and_double=True,
+    #                                          enabled_precisions={torch.half, torch.float32})
+    trt_model_fp_32.save('mobilenet_32.ts')
     # torch.jit.save(trt_model_fp_32, './mobilenet_32.ts')
     trt_model_fp_32 = torch.load('./mobilenet_32.ts')
     # trt_model_fp_16.save('mobilenet_16.ts')
-    random_input = torch.randn((1, 3, 192, 224), dtype=torch.float32)
-    # print("For original model")
-    # benchmark(model)
+    random_input = torch.randn((1, 3, 192, 224), dtype=torch.float32).cuda()
+    print("For original model")
+    benchmark(model)
     print("For tensorrt 32")
     trt_model_fp_32(random_input)
     benchmark(trt_model_fp_32)
